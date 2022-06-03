898a158ef8c7d7fb8ab731c05ec67a162638c710
bbkes <41876322+boxuantomzhao@users.noreply.github.com>
2022-06-03 15:51:29 -0400

    Third commit on PC

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ print("Now let's test how to push and commit in the same step")
 
 #This is after I cloned it on my laptop
 print("testing this line of code is from the lenovo laptop")
+
+#This is another update on my PC
+print("testing what the PC name will be on github")
