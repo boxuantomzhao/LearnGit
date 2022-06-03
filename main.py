33ed8da8169a5963595ebf6c48b1c37dc866b436
b42zhao_laptop <b42zhao_laptop@gmail.com>
2022-06-03 15:39:55 -0400

    edits from laptop

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("Hello I'm trying to learn Github - This is on my local PC")
 print("Now I have a repo set-up in Git, testing how to make changes and commit then push")
 print("Now let's test how to push and commit in the same step")
+
+#This is after I cloned it on my laptop
+print("testing this line of code is from the lenovo laptop")
