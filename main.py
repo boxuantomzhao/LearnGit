a5212d6b0d01a85593f0fb4f29f7990fc651813d
bbkes <41876322+boxuantomzhao@users.noreply.github.com>
2022-06-03 11:41:48 -0400

    Second Commit: after setting up the repos on GIt and Github

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello I'm trying to learn Github - This is on my local PC")
 print("Now I have a repo set-up in Git, testing how to make changes and commit then push")
+print("Now let's test how to push and commit in the same step")
